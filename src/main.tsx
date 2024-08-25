ccb100429b53dd57c24da138c43f7b917e8ec0a6
ToastedToast <hey@toasted.dev>
2024-08-25 11:48:32 +0800

    feat: add tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,27 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+  RouterProvider,
+  createMemoryHistory,
+  createRouter,
+} from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const memoryHistory = createMemoryHistory({
+  initialEntries: ["/"],
+});
+
+const router = createRouter({ routeTree, history: memoryHistory });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
